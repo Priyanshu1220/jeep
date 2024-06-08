13d454f35023916fabd61257b84c051c2530a508
Priyanshu1220 <166621477+Priyanshu1220@users.noreply.github.com>
2024-06-08 12:21:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let menuList = document.getElementById("nav");
+menuList.style.maxHeight = "0px";
+
+function toggleMenu() {
+  if (menuList.style.maxHeight == "0px") {
+    menuList.style.maxHeight = "300px";
+  } else {
+    menuList.style.maxHeight = "0px";
+  }
+}
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
